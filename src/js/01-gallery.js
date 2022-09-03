6903d8386ca08a5049a45e9ecd91c1ffc202a0a7
SemSlipchenko <semslipchenko@gmail.com>
2022-09-03 20:28:12 +0300

    import css

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,6 +1,7 @@
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 import SimpleLightbox from 'simplelightbox/dist/simple-lightbox.esm';
+import "simplelightbox/dist/simple-lightbox.min.css";
 // Change code below this line
 
 const gallery = document.querySelector('.gallery');
