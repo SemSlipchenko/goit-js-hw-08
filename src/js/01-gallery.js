ace9ce3c3c2a296ff8f5ab4be3cf296fc0057207
SemSlipchenko <semslipchenko@gmail.com>
2022-09-03 20:22:15 +0300

    nmp install simplelightbox

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,20 @@
 // Add imports above this line
 import { galleryItems } from './gallery-items';
+import SimpleLightbox from 'simplelightbox/dist/simple-lightbox.esm';
 // Change code below this line
 
-console.log(galleryItems);
+const gallery = document.querySelector('.gallery');
+gallery.insertAdjacentHTML('beforeend', createMarkup(galleryItems));
+
+function createMarkup(items) {
+    return items.map(({preview, original, description}) => { 
+        return `<a class="gallery__item" href="${original}">
+        <img class="gallery__image" src="${preview}" alt="${description}"/></a>`;
+    }).join('');
+};
+
+new SimpleLightbox('.gallery a', { 
+    captionDelay: 250,
+    captionsData: "alt",
+    captionPosition: "bottom",
+});
