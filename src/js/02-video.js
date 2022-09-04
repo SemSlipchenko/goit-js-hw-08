49fd2e610e414c99d8483c3ae77cc84f323de4cb
SemSlipchenko <semslipchenko@gmail.com>
2022-09-04 13:17:55 +0300

    import vimeo

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,10 +1,12 @@
+import Vimeo from '@vimeo/player';
+
 const iframe = document.querySelector('iframe');
-    const player = new Vimeo.Player(iframe);
+const player = new Vimeo.Player(iframe);
 
-    player.on('play', function() {
-        console.log('played the video!');
-    });
+player.on('play', function() {
+    console.log('played the video!');
+});
 
-    player.getVideoTitle().then(function(title) {
-        console.log('title:', title);
-    });
+player.getVideoTitle().then(function(title) {
+    console.log('title:', title);
+});
