b3159fa8d1992dfaaa608794fde14c41d14d1e00
SemSlipchenko <semslipchenko@gmail.com>
2022-09-06 14:04:35 +0300

    fix bag on vimeo player

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -10,5 +10,6 @@ function onPlay(timeupdate) {
     let timeStop = timeupdate.seconds;
     localStorage.setItem("videoplayer-current-time", timeStop);
 };
-
-player.setCurrentTime(localStorage.getItem("videoplayer-current-time"));
+if (!localStorage.getItem("videoplayer-current-time")) {
+    player.setCurrentTime(0);
+} else { player.setCurrentTime(localStorage.getItem("videoplayer-current-time"))};
